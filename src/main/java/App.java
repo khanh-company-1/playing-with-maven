af5a5faefc3188f3d227175c74a4989bde1f8581
Khanh Nguyen <khanh.nguyenduy@axonactive.com>
2024-12-25 10:46:38 +0700

    Init project

---
+++ src/main/java/App.java
@@ -0,0 +1,7 @@
+package com.khanhcompany1.playingwithmaven;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hellooooo, Maven Release Plugin! Here version 1.0.0");
+    }
+}
