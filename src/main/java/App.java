8dd7ba6ab56b69705b057c98c2e919e47cb97934
Khanh Nguyen <119989010+ndkhanh-axonivy@users.noreply.github.com>
2024-12-26 14:23:45 +0700

    Update App.java (#19)

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,7 +3,7 @@ package com.khanhcompany1.playingwithmaven;
 public class App {
     public static void main(String[] args) {
         int a = 10;
-        int b = 20;
+        int b = 30;
         int sum = a + b;
         System.out.println("Sum of a and b is: " + sum);
         System.out.println("Hellooooo, Maven Release Plugin! Here version 1.0.0");
