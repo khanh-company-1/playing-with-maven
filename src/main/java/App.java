93b7ce6be54e5b02845bada361b7d742e8860c5d
Khanh Nguyen <khanh.nguyenduy@axonactive.com>
2024-12-27 01:25:49 +0700

    create release 10

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,7 @@ package com.khanhcompany1.playingwithmaven;
 
 public class App {
     public static void main(String[] args) {
+        System.out.println("Hellooooo, Maven Release Plugin! Here branch release 10.0");
         System.out.println("Hellooooo, Maven Release Plugin! Here version 2.0");
     }
 }
