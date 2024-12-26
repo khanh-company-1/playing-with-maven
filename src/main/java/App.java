741e79fb31dac5a1cbee71bd55718e8c19986a1d
Khanh Nguyen <khanh.nguyenduy@axonactive.com>
2024-12-26 14:21:33 +0700

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,10 @@ package com.khanhcompany1.playingwithmaven;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hellooooo, Maven Release Plugin! Here version 2.0");
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("Sum of a and b is: " + sum);
+        System.out.println("Hellooooo, Maven Release Plugin! Here version 1.0.0");
     }
 }
